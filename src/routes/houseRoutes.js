7ad8b5cd4ef76b1ae48a3ca544e14c8039aed8b0
Bernardo Marques <sardinhaber@gmail.com>
2025-04-01 09:26:36 -0300

    feat: finish project

--- src/routes/houseRoutes.js
+++ src/routes/houseRoutes.js
@@ -4,5 +4,8 @@ const houseController = require("../controllers/houseController");
 
 router.get("/", houseController.getAllHouses);
 router.get("/:id", houseController.getHouse);
+router.post("/", houseController.createHouse);
+router.delete("/:id", houseController.deleteHouse);
+router.put("/:id", houseController.updateHouse);
 
 module.exports = router;
