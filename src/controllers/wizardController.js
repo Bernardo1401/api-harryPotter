6f0becb556adea77ee7808f4062d670b318180fa
Bernardo <sardinhaber@gmail.com>
2025-04-15 14:33:51 -0300

    feat: create swagger and upload img

--- src/controllers/wizardController.js
+++ src/controllers/wizardController.js
@@ -24,12 +24,14 @@ const getWizard = async (req, res) => {
 const createWizard = async (req, res) => {
     try {
         const { name, house_id } = req.body;
-        const newWizard = await wizardModel.createWizard(name, house_id);
+        const photo = req.file ? req.file.filename : null;
+        const newWizard = await wizardModel.createWizard(name, house_id, photo);
         res.status(201).json(newWizard);
     } catch (error) {
         res.status(500).json({ message: "Erro ao criar bruxo." });
     }
 };
+
 
 const deleteWizard = async (req, res) => {
     try {
@@ -52,5 +54,7 @@ const updateWizard = async (req, res) => {
         res.status(500).json({ message: "Erro ao atualizar bruxo." });
     }
 };
+
+
 
 module.exports = { getAllWizards, getWizard, createWizard, deleteWizard, updateWizard };
